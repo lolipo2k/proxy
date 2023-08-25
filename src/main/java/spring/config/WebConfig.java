514b96ad69d91cc90150b6ff4c23a57251ee84d0
lolipo2k <andreasyagin@yandex.ru>
2023-08-25 14:33:02 +0300

    fix origins

--- src/main/java/spring/config/WebConfig.java
+++ src/main/java/spring/config/WebConfig.java
@@ -6,5 +6,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedHeaders("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                    .maxAge(-1)   // add maxAge
+                    .allowCredentials(false);
   }
