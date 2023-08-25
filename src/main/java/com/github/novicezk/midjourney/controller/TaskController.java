d2b01ced909370126c573091a0aab61b32204356
lolipo2k <andreasyagin@yandex.ru>
2023-08-25 15:31:16 +0300

    import corn

--- src/main/java/com/github/novicezk/midjourney/controller/TaskController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collections;
